65ece9ac6ca94622edb4efed1c17c711da2c8abf
sk-5 <sikofitt@gmail.com>
2015-07-15 15:07:18 -0700

    changed example.cpp

--- examples/example.cpp
+++ examples/example.cpp
@@ -11,9 +11,9 @@ int main(void) {
    const char* str = "My number is";
    const char* str2 = "|03My number is|11|30";
    int len = pcprintf("|01%s |41|09|30%d|39\n", str, num);
-   pcprintf("|10Length of Len is : |15|30%d\n|07", len);
+   pcprintf("|02Length of Len is : |10|30%d\n|07", len);
    int len2 = pcsprintf(buffer, "%s %d|39\n", str2, num);
    printf("%s", buffer);
-   pcprintf("|10Length of Len2 is : |15|30%d\n|07", len2);
+   pcprintf("|02Length of Len2 is : |10|30%d\n|07", len2);
    return 0;                  
 }
