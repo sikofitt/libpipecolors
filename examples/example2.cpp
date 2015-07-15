24bbfed64d5ce98d9a33fe257fc7ac9da0ee4010
= <=>
2015-07-14 18:04:34 -0700

    Fixes

--- examples/example2.cpp
+++ examples/example2.cpp
@@ -10,7 +10,7 @@ int main(void) {
    int num = 5;
    const char* str = "My number is";
    const char* str2 = "|10My number is|07";
-   int len = pcprintf("|01%s |41|09%d|39\n", str, num);
+   int len = pcprintf("|01%s |09%d|39\n", str, num);
    pcprintf("|10Length of Len is : |15|30%d\n|07", len);
    int len2 = pcsprintf(buffer, "%s %d\n", str2, num);
    printf("\n%s %d\n", buffer, len2);
