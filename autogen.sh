9a986cb97b95fbed2dfee772e7e6d16ac4174d6b
sk-5 <sikofitt@gmail.com>
2015-08-07 12:39:25 -0700

    ...

--- autogen.sh
+++ autogen.sh
@@ -11,4 +11,4 @@ libtoolize --install --copy || exit 1
 aclocal -I config || exit 1
 autoheader || exit 1
 autoconf || exit 1
-automake --add-missing --copy || exit 1
+automake --add-missing --copy --force || exit 1
