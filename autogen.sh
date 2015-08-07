cfc2e4d4fce72829f3b6f73f758e4cf36760b7a1
sk-5 <sikofitt@gmail.com>
2015-08-07 12:48:25 -0700

    good god

--- autogen.sh
+++ autogen.sh
@@ -6,9 +6,9 @@ test -f configure && autoreconf -i && exit 0 ||
 
 test -d autom4te.cache && rm -rf autom4te.cache
 test -d config && rm -rf config
-mkdir config || exit 1
-libtoolize --install --copy || exit 1
-aclocal -I config || exit 1
-autoheader || exit 1
-autoconf || exit 1
-automake --add-missing --copy --force || exit 1
+mkdir config
+libtoolize --install --copy
+aclocal -I config
+autoheader
+autoconf 
+automake --add-missing --copy --force
